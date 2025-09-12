b848671303f4c1ec20f9421564bde94a1cd23045
ameeqkhan <ameeqkhan183@gmail.com>
2025-09-13 01:44:23 +0500

    asdfsd

--- resources/views/pages/domain-registration.blade.php
+++ resources/views/pages/domain-registration.blade.php
@@ -7,14 +7,10 @@
         <!--hero section start-->
         <x-hero-equal-height title="Domain Registration & Management" :showRightImage="false" :showPriceBox="false"
             subtitle="
-        Find cheap domain names for your website
-        Eazyjungle brings the most competitive prices for domain names
-        Our mission is to provide tailored solutions for your personal or business needs
-        We will not just register a domain for you - We will set it up for you and make sure it is functional by all means
-        Take advantage of our Hosting, Email and SSL solutions to set up your online presence all under one
-    "
-            buttonText="Choose a Domain Name"
-            buttonUrl="https://eazyjungle.com/ezclient/order.php?step=1&productGroup=2" backgroundImage="img/hero-14.jpg" />
+            Find cheap domain names for your website Eazyjungle brings the most competitive prices for domain names Our mission is to provide tailored solutions for your personal or business needs We will not just register a domain for you - We will set it up for you and make sure it is functional by all means Take advantage of our Hosting, Email and SSL solutions to set up your online presence all under one
+            "
+            buttonText="Choose a Domain Name" buttonUrl="https://eazyjungle.com/ezclient/order.php?step=1&productGroup=2"
+            backgroundImage="img/hero-14.jpg" />
 
 
 
@@ -40,7 +36,7 @@
         <!--faq section end-->
 
         <!--testimonial and review section start-->
-       @include('components.client-review')
+        @include('components.client-review')
         <!--testimonial and review section end-->
 
     </div>
