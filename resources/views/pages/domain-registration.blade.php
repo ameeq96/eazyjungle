be471322984b2ef3a4a51c3ca1f4bf50fc21b56b
ameeqkhan <ameeqkhan183@gmail.com>
2025-09-13 23:26:16 +0500

    sdf

--- resources/views/pages/domain-registration.blade.php
+++ resources/views/pages/domain-registration.blade.php
@@ -9,7 +9,7 @@
             subtitle="
             Find cheap domain names for your website Eazyjungle brings the most competitive prices for domain names Our mission is to provide tailored solutions for your personal or business needs We will not just register a domain for you - We will set it up for you and make sure it is functional by all means Take advantage of our Hosting, Email and SSL solutions to set up your online presence all under one
             "
-            buttonText="Choose a Domain Name" buttonUrl="https://eazyjungle.com/ezclient/order.php?step=1&productGroup=2"
+            buttonText="Choose a Domain Name" buttonUrl="https://eazyjungle.pk/public/ezclient/order.php?step=1&productGroup=2"
             backgroundImage="img/hero-14.jpg" />
 
 
